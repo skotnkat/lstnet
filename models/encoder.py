9e53e4f1dc11384fdafd0a536ae5813210653219
skotnkat <skot.junior@seznam.cz>
2025-03-18 09:43:40 +0100

    Implement Encoder using LstnetComponent abstract class
    Implement Encoder using LstnetComponent abstract class to reduce duplicate code between derived classes.

---
+++ models/encoder.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import utils
+import math
+
+from models.lstnet_component import LstnetComponent
+from models.extended_layers import Conv2dExtended
+
+
+class Encoder(LstnetComponent):
+    def __init__(self, input_size, in_channels_num, params):
+        super().__init__(input_size, in_channels_num, params)
+
+    
+    @staticmethod
+    def _create_stand_layer(params, in_channels, input_size):        
+        layer = nn.Sequential(
+            Conv2dExtended(in_channels, input_size=input_size, **params),
+            nn.BatchNorm2d(params["out_channels"]),
+            nn.LeakyReLU()
+        )
+    
+        return layer
+
+    @staticmethod
+    def _compute_layer_output_size(layer, input_size):
+        return layer[0].compute_output_size(input_size)
