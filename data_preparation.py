22729a262ee2445c7b72030a4bee6dacf1b483c0
skotnkat <skot.junior@seznam.cz>
2025-04-14 11:34:23 +0200

    Implement loading of dataset
    Based on the name of the dataset, load it and apply transformations.

--- data_preparation.py
+++ data_preparation.py
@@ -23,3 +23,25 @@ def create_augmentation_steps(img_size):
         ToDtype(torch.float32, scale=True)
     ])
 
+
+def load_dataset(dataset_name, train_op=True, transform_steps=BASIC_TRANSFORMATION, **kwargs):
+    if dataset_name == 'MNIST':
+        data = datasets.MNIST(root="./data", train=train_op, transform=transform_steps, **kwargs)
+        print('Loaded MNIST')
+
+    elif dataset_name == 'USPS':
+        data = datasets.USPS(root="./data", train=train_op, transform=transform_steps, **kwargs)
+        print('Loaded USPS')
+
+    elif dataset_name == 'SVHN':
+        # targets are called labels and are array
+        split = 'test'
+        if train_op:
+            split = 'train'
+        data = datasets.SVHN(root="./data", split=split, transform=transform_steps, **kwargs)
+        
+    else:
+        raise ValueError(f"Not able to load dataset {dataset_name}")
+
+    return data
+
