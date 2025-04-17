e0f5d2748fe8647c21c6033b70544958f99d1744
skotnkat <skot.junior@seznam.cz>
2025-04-17 16:08:27 +0200

    Update train.py
    Implement the main training loop.
    1) Initialize Adam optimizers.
    2) Run the training loop until the AG converges (difference in loss between epochs is less than delta loss).
    3) Use alternating gradient - in odd iterations, update weights of discriminators, otherwise weights of encoders and generators.

--- train.py
+++ train.py
@@ -1,5 +1,12 @@
 import numpy as np
+import torch
+from torch.optim import Adam
+import json
+
+from models.lstnet import LSTNET
 from loss_functions import compute_discriminator_loss, compute_enc_gen_loss
+from data_preparation import get_training_loader
+import utils
 
 
 def compute_loss(model, first_domain_batch, second_domain_batch):
@@ -7,3 +14,56 @@ def compute_loss(model, first_domain_batch, second_domain_batch):
     total_enc_gen_loss = compute_enc_gen_loss(model, first_domain_batch, second_domain_batch)
 
     return total_disc_loss, total_enc_gen_loss
+
+
+def run_training(model, loader):
+    """First phase of training. Without knowledge of the labels (will be ignoring the labels)."""
+    disc_params = list(model.first_discriminator.parameters(),
+                       model.second_discriminator.parameters(),
+                       model.latent_discriminator.parameters())
+    optim_disc = Adam(disc_params, lr=utils.ADAM_LR, betas=utils.ADAM_DECAY)
+
+    enc_gen_params = list(model.first_encoder.parameters(),
+                          model.second_encoder.parameters(),
+                          model.shared_encoder.parameters(),
+                          model.first_generator.parameters(),
+                          model.second_generator.parameters(),
+                          model.shared_generator.parameters())
+
+    optim_enc_dec = Adam(enc_gen_params, lr=utils.ADAM_LR, betas=utils.ADAM_DECAY)
+
+    epoch_num = 0
+    converged = False
+    prev_total_loss = np.inf
+    loss_list = []
+    while not converged:
+        total_disc_loss, total_enc_gen_loss = None, None
+        for batch_idx, (first_domain_batch, _, second_domain_batch, _) in enumerate(loader):
+            total_disc_loss, total_enc_gen_loss = compute_loss(model, first_domain_batch, second_domain_batch)
+
+            if batch_idx % 2:  # odd -> update discriminators
+                optim_disc.zero_grad()
+                total_disc_loss.backward()
+                optim_disc.step()
+
+            else:  # update encoders and generators
+                optim_enc_dec.zero_grad()
+                total_enc_gen_loss.backward()
+                optim_enc_dec.step()
+
+            if (batch_idx % 1000):
+                print(f'batch idx: {batch_idx}, disc loss: {total_disc_loss}, enc gen loss: {total_enc_gen_loss}')
+
+        cur_total_loss = total_disc_loss + total_enc_gen_loss
+
+        if torch.abs(cur_total_loss - prev_total_loss) < utils.DELTA_LOSS:
+            converged = True
+
+        loss_list.append(cur_total_loss)
+        prev_total_loss = cur_total_loss
+
+        print(f'End of epoch {epoch_num}, current total loss: {cur_total_loss}')
+        epoch_num += 1
+
+    return model, loss_list
+
