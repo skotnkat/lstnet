d5a7628036726649df9b3167b79ee3ec5fb13a6c
skotnkat <skot.junior@seznam.cz>
2025-04-18 01:23:04 +0200

    Update loss_functions.py
    Set required_grad to False for fixed values used only for comparison.

--- loss_functions.py
+++ loss_functions.py
@@ -9,14 +9,14 @@ cycle_loss = nn.L1Loss()
 
 def adversarial_loss_real(batch):
     batch_size = batch.size(0)
-    ones_labels = torch.ones(batch_size, 1, device=batch.device)  # expecting to be real
+    ones_labels = torch.ones(batch_size, 1, device=batch.device, requires_grad=False)  # expecting to be real
     
     return adversarial_loss(batch, ones_labels)
     
 
 def adversarial_loss_gen(batch):
     batch_size = batch.size(0)
-    zeros_labels = torch.zeros(batch_size, 1, device=batch.device)  # expecting to be real
+    zeros_labels = torch.zeros(batch_size, 1, device=batch.device, requires_grad=False)  # expecting to be real
 
     return adversarial_loss(batch, zeros_labels)
 
