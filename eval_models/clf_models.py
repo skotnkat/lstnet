0d5466be35a48eb2b7590556b9152d05be964421
skotnkat <skot.junior@seznam.cz>
2025-04-22 18:08:36 +0200

    Implement SVHN Classification Task

--- eval_models/clf_models.py
+++ eval_models/clf_models.py
@@ -47,3 +47,40 @@ class UspsClf(Discriminator):
 
         return last_layer
 
+
+class SvhnClf(Discriminator):
+    def __init__(self, params):
+        self.input_size = (16, 16)
+        self.in_channels_num = 1
+        super().__init__(self.input_size, self.in_channels_num, params)
+
+        self.optimizer = optim.Adam(self.layers.parameters(), lr=0.001)
+        self.criterion = nn.CrossEntropyLoss()
+        self.epochs = 50
+        self.patience = 10
+
+    @staticmethod
+    def _create_stand_layer(params, in_channels, input_size):
+        conv_params, activation_params, pool_params = params
+        conv = Conv2dExtended(in_channels, input_size=input_size, **conv_params)
+        batch_norm = nn.BatchNorm2d(params["out_channels"], momentum=0.01, eps=0.001)
+        relu = nn.ReLU()
+
+        layers = [conv, batch_norm, relu]
+
+        if len(pool_params):
+            output_size = conv.compute_output_size(input_size)
+            layers.append(MaxPool2dExtended(input_size=output_size, **pool_params))
+            layers.append(nn.Dropout(0.3))
+
+        return nn.Sequential(*layers)
+
+    @staticmethod
+    def _crete_last_layer(params):
+        last_layer = nn.Sequential(
+            nn.Flatten(),
+            nn.Dropout(0.3),
+            nn.Linear(**params)
+        )
+
+        return last_layer
