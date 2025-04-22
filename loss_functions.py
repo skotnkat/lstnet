1ee12015fc11224a15e777296c50952ddbdc51d1
Katerina Skotnicova <skotnkat@fit.cvut.cz>
2025-04-22 19:27:42 +0200

    Correct encoder and generator loss functions
    Compute total encoder generator loss function by summing them.
    
    Correct the order of cycle consistent loss components.
    
    Make encoder maximize the probability of latent discriminator being unsure of which domain the latent representation is. By that  the latent representations meet in the middle.

--- loss_functions.py
+++ loss_functions.py
@@ -46,9 +46,9 @@ def compute_discriminator_loss(model, first_real, second_real, first_gen, second
 
 def compute_cc_loss(first_real, second_real, first_cycle, second_cycle, first_full_cycle, second_full_cycle):
     cc_loss_1 = cycle_loss(first_cycle, first_real)  # (predictions, target)
-    cc_loss_2 = cycle_loss(first_full_cycle, first_real)
+    cc_loss_2 = cycle_loss(second_cycle, second_real)
 
-    cc_loss_3 = cycle_loss(second_cycle, second_real)
+    cc_loss_3 = cycle_loss(first_full_cycle, first_real)
     cc_loss_4 = cycle_loss(second_full_cycle, second_real)
 
     return W_3*cc_loss_1 + W_4*cc_loss_2 + W_5*cc_loss_3 + W_6*cc_loss_4
@@ -61,13 +61,9 @@ def compute_enc_gen_loss(model, first_gen, second_gen, first_latent, second_late
     second_gen_disc = model.second_discriminator.forward(second_gen)
     second_gen_loss = adversarial_loss_real(second_gen_disc)
 
-    first_latent_disc = model.latent_discriminator.forward(first_latent)  # forcing to be second domain
-    first_latent_loss = adversarial_loss_real(first_latent_disc)  # encourage the encoders to make the domains look the same
-
+    first_latent_disc = model.latent_discriminator.forward(first_latent)
     second_latent_disc = model.latent_discriminator.forward(second_latent)
-    second_latent_loss = adversarial_loss_real(second_latent_disc)  # encourage the encoder to fool the discriminator
-
-    latent_loss = (first_latent_loss+second_latent_loss) / 2
+    latent_loss = network_adversarial_loss(second_latent_disc, first_latent_disc) / 2
 
     return W_1*first_gen_loss, W_2*second_gen_loss, W_l*latent_loss
 
