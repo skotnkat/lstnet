5ae23eef125f0544e8aa1870d828780f77e496ac
skotnkat <skot.junior@seznam.cz>
2025-04-25 18:54:53 +0200

    Add global variables for logging component losses

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import json
 
-
 PARAMS_FILE_PATH = None
 NETWORK_NAMES = {"first_encoder", "second_encoder", "shared_encoder", "first_generator", "second_generator",
                  "shared_generator", "first_discriminator", "second_discriminator", "latent_discriminator"}
@@ -18,14 +17,19 @@ NUM_WORKERS = None
 FIRST_INPUT_SHAPE, SECOND_INPUT_SHAPE = None, None
 FIRST_IN_CHANNELS_NUM, SECOND_IN_CHANNELS_NUM = None, None
 
+DISC_LOSSES = {'first_loss': [], 'second_loss': [], 'latent_loss': []}
+CC_LOSSES = {'first_cycle': [], 'second_cycle': [], 'first_full_cycle': [], 'second_full_cycle': []}
+ENC_GEN_LOSSES = {'first_loss': [], 'second_loss': [], 'latent_loss': []}
+
+
 def get_networks_params():
     with open(PARAMS_FILE_PATH, "r") as file:
         params = json.load(file)
-    
+
     missing_network_params = NETWORK_NAMES.difference(set(params.keys()))
     if len(missing_network_params):
         raise ValueError(f"Missing Input Parameters for Network Parts: {missing_network_params}")
-    
+
     return params
 
 
@@ -52,7 +56,7 @@ def transform_int_to_tuple(x):
 def standardize_padding(padding):
     padding = transform_int_to_tuple(padding)
     is_padding_same = (padding == "same")
-    
+
     if not isinstance(padding, tuple):
         padding = (0, 0)
 
@@ -68,3 +72,50 @@ def split_padding(p_total):
 
 def compute_effective_kernel_size(kernel_size, dilation):
     return dilation * (kernel_size - 1) + 1
+
+
+def init_epoch_loss():
+    DISC_LOSSES['first_loss'].append(0)
+    DISC_LOSSES['second_loss'].append(0)
+    DISC_LOSSES['latent_loss'].append(0)
+
+    CC_LOSSES['first_cycle'].append(0)
+    CC_LOSSES['second_cycle'].append(0)
+    CC_LOSSES['first_full_cycle'].append(0)
+    CC_LOSSES['second_full_cycle'].append(0)
+
+    ENC_GEN_LOSSES['first_loss'].append(0)
+    ENC_GEN_LOSSES['second_loss'].append(0)
+    ENC_GEN_LOSSES['latent_loss'].append(0)
+
+
+def log_epoch_loss(disc_loss, enc_gen_loss, cc_loss, cur_epoch):
+    DISC_LOSSES['first_loss'][cur_epoch] += disc_loss[0]
+    DISC_LOSSES['second_loss'][cur_epoch] += disc_loss[1]
+    DISC_LOSSES['latent_loss'][cur_epoch] += disc_loss[2]
+
+    CC_LOSSES['first_cycle'][cur_epoch] += cc_loss[0]
+    CC_LOSSES['first_full_cycle'][cur_epoch] += cc_loss[1]
+    CC_LOSSES['second_cycle'][cur_epoch] += cc_loss[2]
+    CC_LOSSES['second_full_cycle'][cur_epoch] += cc_loss[3]
+
+    ENC_GEN_LOSSES['first_loss'][cur_epoch] += enc_gen_loss[0]
+    ENC_GEN_LOSSES['second_loss'][cur_epoch] += enc_gen_loss[1]
+    ENC_GEN_LOSSES['latent_loss'][cur_epoch] += enc_gen_loss[2]
+
+
+def normalize_epoch_loss(scale, cur_epoch):
+    DISC_LOSSES['first_loss'][cur_epoch] /= scale
+    DISC_LOSSES['second_loss'][cur_epoch] /= scale
+    DISC_LOSSES['latent_loss'][cur_epoch] /= scale
+
+    CC_LOSSES['first_cycle'][cur_epoch] /= scale
+    CC_LOSSES['first_full_cycle'][cur_epoch] /= scale
+    CC_LOSSES['second_cycle'][cur_epoch] /= scale
+    CC_LOSSES['second_full_cycle'][cur_epoch] /= scale
+
+    ENC_GEN_LOSSES['first_loss'][cur_epoch] /= scale
+    ENC_GEN_LOSSES['second_loss'][cur_epoch] /= scale
+    ENC_GEN_LOSSES['latent_loss'][cur_epoch] /= scale
+
+
