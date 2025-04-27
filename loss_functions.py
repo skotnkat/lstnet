8f6170da6a1e55f6bfba330ff7f16ef63475471e
Katerina Skotnicova <skotnkat@fit.cvut.cz>
2025-04-27 13:12:55 +0000

    Introduce label smoothing to the discirminators loss to avoid disc to become too powerful too early. Might help with stability of training.

--- loss_functions.py
+++ loss_functions.py
@@ -7,41 +7,46 @@ adversarial_loss = nn.BCEWithLogitsLoss()
 cycle_loss = nn.L1Loss()
 
 
-def adversarial_loss_real(batch):
+def adversarial_loss_real(batch, label_smoothing=False):
     batch_size = batch.size(0)
-    #ones_labels = torch.ones(batch_size, 1, device=batch.device, requires_grad=False)  # expecting to be real
-    ones_labels = torch.full((batch_size, 1), 0.9, device=batch.device, requires_grad=False)  # avoid discriminators to become too strong
+    ones_labels = torch.ones(batch_size, 1, device=batch.device, requires_grad=False)  # expecting to be real
+    
+    if label_smoothing:
+        ones_labels = torch.full((batch_size, 1), 0.9, device=batch.device, requires_grad=False)  # avoid discriminators to become too strong
 
     return adversarial_loss(batch, ones_labels)
 
 
-def adversarial_loss_gen(batch):
+def adversarial_loss_gen(batch, label_smoothing=False):
     batch_size = batch.size(0)
     zeros_labels = torch.zeros(batch_size, 1, device=batch.device, requires_grad=False)  # expecting to be fake
+
+    if label_smoothing:
+        zeros_labels = torch.full((batch_size, 1), 0.9, device=batch.device, requires_grad=False)  # avoid discriminators to become too strong
 
     return adversarial_loss(batch, zeros_labels)
 
 
-def network_adversarial_loss(batch_real, batch_gen):
-    real_loss = adversarial_loss_real(batch_real)
-    gen_loss = adversarial_loss_gen(batch_gen)
+def network_adversarial_loss(batch_real, batch_gen, label_smoothing=False):
+    real_loss = adversarial_loss_real(batch_real, label_smoothing)
+    gen_loss = adversarial_loss_gen(batch_gen, label_smoothing)
 
-    return real_loss + gen_loss  # normalization
+    return real_loss + gen_loss
 
 
 def compute_discriminator_loss(model, first_real, second_real, first_gen, second_gen, first_latent, second_latent,
                                return_grad=True):
     first_real_disc = model.first_discriminator.forward(first_real)
     first_gen_disc = model.first_discriminator.forward(first_gen)
-    first_disc_loss = network_adversarial_loss(first_real_disc, first_gen_disc)
+    first_disc_loss = network_adversarial_loss(first_real_disc, first_gen_disc, label_smoothing=True)
 
     second_real_disc = model.second_discriminator.forward(second_real)
     second_gen_disc = model.second_discriminator.forward(second_gen)
-    second_disc_loss = network_adversarial_loss(second_real_disc, second_gen_disc)
+    second_disc_loss = network_adversarial_loss(second_real_disc, second_gen_disc, label_smoothing=True)
 
     first_latent_disc = model.latent_discriminator.forward(first_latent)
     second_latent_disc = model.latent_discriminator.forward(second_latent)
-    latent_disc_loss = network_adversarial_loss(first_latent_disc, second_latent_disc)
+    latent_disc_loss = network_adversarial_loss(first_latent_disc, second_latent_disc, label_smoothing=True)
 
     if return_grad:
         return W_1 * first_disc_loss, W_2 * second_disc_loss, W_l * latent_disc_loss
