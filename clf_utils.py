bf587d6878e93496fff25cf48f0c8949b5845cfa
Katerina Skotnicova <skotnkat@fit.cvut.cz>
2025-06-03 20:05:22 +0200

    Add Base Clf Class

--- clf_utils.py
+++ clf_utils.py
@@ -1,18 +1,21 @@
 import utils
-from eval_models.clf_models import MnistClf, UspsClf, SvhnClf
+from eval_models.clf_models import BaseClf, MnistClf, UspsClf
 
 
-def select_classifier(domain_name, params):
+def select_classifier(domain_name, input_size, in_channels, params, leaky_relu, custom_clf=False):
     clf = None
 
-    if domain_name == "MNIST":
+    if not custom_clf:
+        clf = BaseClf(input_size, in_channels, params, negative_slope=leaky_relu)
+
+    elif domain_name == "MNIST":
         clf = MnistClf(params)
 
     elif domain_name == "USPS":
         clf = UspsClf(params)
 
-    elif domain_name == "SVHN":
-        clf = SvhnClf(params)
+    # elif domain_name == "SVHN":
+    #     clf = SvhnClf(params)
 
     if clf is None:
         raise ValueError("No classifier model as loaded.")
--- eval_models/clf_models.py
+++ eval_models/clf_models.py
@@ -4,6 +4,21 @@ from models.extended_layers import MaxPool2dExtended, Conv2dExtended
 import torch.optim as optim
 
 
+class BaseClf(Discriminator):
+    def __init__(self, input_size, in_channels, params, optimizer=None, epochs=50, patience=5, negative_slope=0.01):
+        self.input_size = tuple(input_size)
+        self.in_channels_num = in_channels
+
+        super().__init__(self.input_size, self.in_channels_num, params, negative_slope=negative_slope)
+
+        self.criterion = nn.CrossEntropyLoss()
+
+        # default parameters can be overwritten
+        self.optimizer = optimizer
+        self.epochs = epochs
+        self.patience = patience
+
+
 class MnistClf(Discriminator):
     def __init__(self, params):
         self.input_size = (28, 28)
@@ -50,40 +65,40 @@ class UspsClf(Discriminator):
         return last_layer
 
 
-class SvhnClf(Discriminator):
-    def __init__(self, params):
-        self.input_size = (16, 16)
-        self.in_channels_num = 1
-        super().__init__(self.input_size, self.in_channels_num, params)
-
-        self.optimizer = optim.Adam(self.layers.parameters(), lr=0.001)
-        self.criterion = nn.CrossEntropyLoss()
-        self.epochs = 50
-        self.patience = 10
-
-        self.dropout_p = params["dropout_p"]
-
-    def _create_stand_layer(self, params, in_channels, input_size):
-        conv_params, pool_params = params
-        conv = Conv2dExtended(in_channels, input_size=input_size, **conv_params)
-        batch_norm = nn.BatchNorm2d(params["out_channels"], momentum=0.01, eps=0.001)
-        relu = nn.ReLU()
-
-        layers = [conv, batch_norm, relu]
-
-        if len(pool_params):
-            output_size = conv.compute_output_size(input_size)
-            layers.append(MaxPool2dExtended(input_size=output_size, **pool_params))
-            layers.append(negative_slope=self.dropout_p)
-
-        return nn.Sequential(*layers)
-
-    def _create_last_layer(self):
-        last_layer = nn.Sequential(
-            nn.Flatten(),
-            nn.Dropout(self.dense_layer_params["dropout_p"]),
-            nn.Linear(in_features=self.dense_layer_params["in_features"],
-                      out_features=self.dense_layer_params["out_features"])
-        )
-
-        return last_layer
+# class SvhnClf(Discriminator):
+#     def __init__(self, params):
+#         self.input_size = (32, 32)
+#         self.in_channels_num = 3
+#         self.global_params = params[-1]
+#         super().__init__(self.input_size, self.in_channels_num, params[:-1])
+#
+#         self.optimizer = optim.Adam(self.layers.parameters(), lr=0.001)
+#         self.criterion = nn.CrossEntropyLoss()
+#         self.epochs = 50
+#         self.patience = 10
+#
+#
+#     def _create_stand_layer(self, params, in_channels, input_size):
+#         conv_params, pool_params = params
+#         conv = Conv2dExtended(in_channels, input_size=input_size, **conv_params)
+#         batch_norm = nn.BatchNorm2d(conv_params["out_channels"], momentum=self.global_params["batch_norm_momentum"])
+#         relu = nn.ReLU()
+#
+#         layers = [conv, batch_norm, relu]
+#
+#         if len(pool_params):
+#             output_size = conv.compute_output_size(input_size)
+#             layers.append(MaxPool2dExtended(input_size=output_size, **pool_params))
+#             layers.append(negative_slope=self.global_params["leaky_relu_neg_slope"])
+#
+#         return nn.Sequential(*layers)
+#
+#     def _create_last_layer(self):
+#         last_layer = nn.Sequential(
+#             nn.Flatten(),
+#             nn.Dropout(self.dense_layer_params["dropout_p"]),
+#             nn.Linear(in_features=self.dense_layer_params["in_features"],
+#                       out_features=self.dense_layer_params["out_features"])
+#         )
+#
+#         return last_layer
