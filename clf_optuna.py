195f332d81cd16872a1aceb342403c01bf2ba805
Katerina Skotnicova <skotnkat@fit.cvut.cz>
2025-06-08 08:26:55 +0200

    Optuna Clf: save trained models
    Update saving and file handling of the best models.

--- clf_optuna.py
+++ clf_optuna.py
@@ -9,6 +9,7 @@ import json
 import train_eval_clf
 import clf_utils
 import torch
+import shutil
 
 
 def parse_args():
@@ -83,12 +84,19 @@ def objective(trial, domain_name, input_size, in_channels, custom_clf):
 
     trained_clf, val_acc, results = train_eval_clf.train(clf, train_loader, val_loader, run_optuna=True, trial=trial)
 
+    model_path = f"{output_dir}/clf_model_trial_{trial.number}.pth"
+    torch.save(trained_clf, model_path)
+
+    trial.set_user_attr("model_path", model_path)
+
     return val_acc
 
 
 if __name__ == "__main__":
     utils.assign_device()
     args = parse_args()
+    output_dir = f"optuna_results/{args.domain_name}"
+    os.makedirs(output_dir, exist_ok=True)
 
     pruner = optuna.pruners.MedianPruner(n_startup_trials=10, interval_steps=1)
 
@@ -99,18 +107,30 @@ if __name__ == "__main__":
     study.optimize(lambda trial: objective(trial, args.domain_name, args.input_size, args.in_channels, args.custom_clf),
                    n_trials=50)
 
-    trial = study.best_trial
-    best_val_acc = trial.value
-    best_params = trial.params
+    best_trial = study.best_trial
+    best_val_acc = best_trial.value
+    best_params = best_trial.params
+    best_model_path = best_trial.user_attrs["model_path"]
 
     print(f'Best validation accuracy: {best_val_acc:.4f}')
     print(f'Best params:')
     for key, value in best_params.items():
         print(f"\t{key}: {value}")
 
-    output_dir = f"optuna_results/{args.domain_name}"
-    os.makedirs(output_dir, exist_ok=True)
-
     best_params["best_val_acc"] = best_val_acc
     with open(f"{output_dir}/params.json", "w") as file:
         json.dump(best_params, file, indent=2)
+
+    shutil.copy(best_model_path, f"{output_dir}/best_model.pth")
+    # Cleanup: remove all other .pth files except best_model.pth
+    for filename in os.listdir(output_dir):
+        path = os.path.join(output_dir, filename)
+        if filename.endswith(".pth") and os.path.abspath(path) not in [
+            os.path.abspath(best_model_path),
+            os.path.abspath(f"{output_dir}/best_model.pth")
+        ]:
+            try:
+                os.remove(path)
+                print(f"Deleted: {path}")
+            except Exception as e:
+                print(f"Error deleting {path}: {e}")
