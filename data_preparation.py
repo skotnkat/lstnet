14f25b9c05244ab05fb419e9fc93b3d83007f37c
Kateřina Skotnicová <skotnkat@fit.cvut.cz>
2025-12-27 15:51:07 +0100

    Update data_preparation.py

--- data_preparation.py
+++ data_preparation.py
@@ -557,7 +557,7 @@ def get_dataset_chw(
 def get_svhn_extra_dataset():
     transform_steps = create_transform_steps(3)
     data = datasets.SVHN(
-        root="./data", split="extra", transform=transform_steps
+        root="./data", split="extra", transform=transform_steps, download=True
     )
     
     return data
